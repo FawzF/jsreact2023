cb6a64c160bbac633267335dca62443c6b2fccec
FawzF <semyonzhukov638@gmail.com>
2023-05-01 06:23:38 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 // import './mycss.css'
 // import * as classstyle from './mycs.module.css'
-import React, { useEffect, useState } from 'react';
-import {  Card } from 'antd'
+import React, { useEffect, useState, useReducer } from 'react';
+import {  Card, Col, Row } from 'antd'
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./pages/welcome";
+import User from "./pages/user";
+import { useSelector } from "react-redux";
+
 
 function App() {
   const [counter, setCounter] = useState()
   const [users, setUsers] = useState([])
-
+  const [userPosts, setPosts] = useState([])
 
   const increment = () => {
     setCounter(counter+1)
@@ -27,6 +33,14 @@ function App() {
         }
       })
 
+      fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(resPost => resPost.json())
+      .then(resPost => {
+        if (resPost && Array.isArray(resPost) && resPost.length > 0) {
+          setPosts(resPost)
+        } 
+      })
+
   }
 
   const loadUsers = () => {
@@ -39,6 +53,14 @@ function App() {
 
   return(
     <>
+        <>
+        <Routes>
+         <Route element={<Welcome />} path='/' />
+         <Route element={<User />} path='/user'/>
+        </Routes>
+
+        </>
+
       <h2>  Users: <button type="" onClick={() => {loadUsers()}}>Load users</button></h2>
       <div style={{margin: 50, display: 'grid', gap: 16}}>
         {users.length > 0 &&
@@ -49,11 +71,31 @@ function App() {
               <p  style={{width: 300, margin:10}}><strong style = {{color: '#6A5ACD'}}>Phone:</strong> {user.phone}</p>
               <p  style={{width: 300, margin:10}}><strong style = {{color: '#006400'}}>Website:</strong> {user.website}</p>
               
+              <Row gutter={10}>  
+               { 
+                
+                userPosts.filter(post => post.userId === user.id).map(post =>{
+                  return(
+                    
+                    <Col span={30}>
+                    <Card title={post.title}  bordered={false} style={{margin:10}} headStyle={{backgroundColor: '#FFE4B5'}}>
+                    <p >{post.body} </p>
+                    </Card>
+                    </Col>
+                    
+                      ) 
+                
+                    } 
+                  )
+                
+              }
+              </Row>
               </Card>
               )
           })
         }
       </div>
+
     </>
   )
 }
